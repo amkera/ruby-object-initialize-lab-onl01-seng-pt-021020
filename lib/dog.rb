5a62a9ebdac19e895147de89a4e95b0ce066dae6
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-26 04:10:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    
+    if breed
+      @breed
+    else
+      @breed = "Mutt"
+    end
+      
   end
   
 end
