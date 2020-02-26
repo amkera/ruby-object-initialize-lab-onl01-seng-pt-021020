647c68af338a31a46dbc0fdad60674e9c43b9d17
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-26 04:23:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,14 @@
 class Person
-    def initialize(name)
-        @name = name
+    def initialize(persons_name) #initialize method
+        @name = persons_name
     end
+
+    def name=(persons_name) #writer method
+        @name = persons_name
+    end
+
+    def name
+        @name #reader method
+    end
+
 end
-
-
